eca71b4fff0e47158fa0d8be109c5c1151e2268b
caifen <1224758467@qq.com>
2018-09-08 16:58:30 +0800

    修改api

---
+++ fcc02/js/index2.js
@@ -0,0 +1,72 @@
+$(function() {
+    $.ajax({
+        type: "GET",
+        url: "https://www.tianqiapi.com/api/",
+        data: "version=v1",
+        dataType: "JSON",
+        success: function(res) {
+            $("#localCity").text(res.city + ' ');
+            $("#weather-local p span").eq(0).append(' ' + res.data[0].date + ' ' + res.data[0].week);
+            $("#weather-local li").eq(0).append(' ' + res.data[0].air + ' ' + res.data[0].air_level);
+            $("#weather-local li").eq(1).append(' ' + res.data[0].index[3].level + ' &nbsp;&nbsp;' + res.data[0].index[3].desc);
+            $("#weather-local li").eq(2).append(' ' + res.data[0].index[4].level + ' &nbsp;&nbsp;' + res.data[0].index[4].desc);
+            $("#weather-local li").eq(4).append(' ' + res.data[0].index[1].desc);
+            $("#weather-local li").eq(5).append(' ' + res.data[0].index[0].level + ' &nbsp;&nbsp;' + res.data[0].index[0].desc);
+
+            var weaClass;
+            // 遍历数组
+            for (var i = 0; i < res.data[0].hours.length; i++) {
+                weaClass = getWeaIcon(getWea(res.data[0].hours[i].wea));
+                $('#hours').append('<li>'  + res.data[0].hours[i].day + '  ' + res.data[0].hours[i].tem +  ' ' + res.data[0].hours[i].wea + ' <span class=icon-'+ weaClass +'> </span> </li >');
+            }
+
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+        
+
+    });
+
+    //獲取天氣描述
+    function getWea(desc) {
+        var index;
+        var weather = ['小雨', '阵雨', '多云'];
+        if(weather.indexOf(desc) !== -1) {
+            index = weather.indexOf(desc);
+        }
+        return index;
+    }
+
+    //获取对应的图标
+    function getWeaIcon(index) {
+        var className = ['rainy3','rainy2','cloudy2'];
+        return className[index];
+    }
+
+    $('#ser').on('click', function() {
+        var inpValue = $('#weather-search input').eq(0).val();
+        
+
+        //查询该城市天气
+        $.ajax({
+            type: "GET",
+            url: "https://www.tianqiapi.com/api/",
+            data: "version=v1&city=" + inpValue,
+            dataType: "JSON",
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log(errorThrown);
+            },
+            success: function(res) {
+                $("#serCity").text(res.city + ' ');
+                $("#ser-result p span").eq(0).text('空气质量 : ' + res.data[0].date + ' ' + res.data[0].week);
+                $("#ser-result li").eq(0).text('穿衣指数 : ' + res.data[0].air + ' ' + res.data[0].air_level);
+                $("#ser-result li").eq(1).text('洗车指数 :' + res.data[0].index[3].level + ' ' + res.data[0].index[3].desc);
+                $("#ser-result li").eq(2).text('旅行指数 : ' + res.data[0].index[4].level + ' ' + res.data[0].index[4].desc);
+                $("#ser-result li").eq(4).text('锻炼指数 : ' + res.data[0].index[1].desc);
+                $("#ser-result li").eq(5).text('紫外线指数 :' + res.data[0].index[0].level + ' ' + res.data[0].index[0].desc);
+
+            }
+        });
+    });
+});
---
+++ fcc02/readme.md
@@ -0,0 +1,11 @@
+<h1>天气预报</h1>
+<pre>
+1、练习来源：<a href="https://freecodecamp.cn/challenges/show-the-local-weather">Show the Local Weather</a>
+2、天气api：https://www.tianqiapi.com/ （支持获取当前城市）
+   apixu ：https://www.apixu.com/doc/   （可以github关联后使用，英文，仅天气描述可返回中文，且有天气图片路径）
+3、JQuery（ajax 或是 getJSON）
+4、预览效果：<a href="https://caifu23.github.io/myFCC/fcc02/">当地天气</a>
+            <a href="https://caifu23.github.io/myFCC/fcc02/index2.html">当地天气</a>
+5、待完善：
+   样式及js代码的处理（冗余）
+</pre>
