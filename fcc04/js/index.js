f085642b40396267ef614dc2561fb15013680473
caifen <1224758467@qq.com>
2018-09-10 21:05:28 +0800

    将是否在直播状态遍历结束后再显示

--- fcc04/js/index.js
+++ fcc04/js/index.js
@@ -4,29 +4,40 @@ $(function() {
     var tvChannels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","esl_sc2"];
     var streamApi = "https://wind-bow.glitch.me/twitch-api/streams/";
     var channelApi = 'https://wind-bow.glitch.me/twitch-api/channels/';
-    var arr = [];
-
-    // $.each(tvChannels, function(k, v) {
-    //     allStreamCall(v);   
-    // });
+    var arr = {};
 
     // 遍历tvChannels
     tvChannels.forEach(function(v) {
         allStreamCall(v);
+        
     });
-
-    console.log(arr);
 
     function allStreamCall(v) {
         //是否正在直播
         $.getJSON(streamApi + v + "?callback=?",function(res){
-            if(res.stream == null) {
+            if(res.stream == null || res.stream === undefined) {
                 arr[v] = 'offline';
             }else {
                 arr[v] = 'inline';
             }
         });
-        // 节目的相关信息
+        
+     
+
+    }
+
+
+    var flag = true;
+    console.log(arr);
+    for(var key in arr) {
+        console.log(arr[key] + '==')
+        if(arr[key] == 'undefined'){
+            flag = false;
+        }
+    }
+    if(flag) {
+        tvChannels.forEach(function(v) {
+          // 节目的相关信息
         $.getJSON(channelApi + v + "?callback=?",function(data){
             console.log(arr[v]  + '-------' + v);
             if(arr[v] == 'inline') {
@@ -35,10 +46,12 @@ $(function() {
             }else {
                 $('#tvList').append(" <li class='row "+ arr[v] +"'><div class='col-md-2' ><img src='" + data.logo +  "'/></div><div class='col-md-4' ><a href='"  + data.url +  "' target='_blank'>"+ data.display_name +"</a></div><div class='col-md-6'>" + "offline" + "</div></li>");
             }
-        });
-     
-
+        });  
+            
+        });  
     }
+
+    
 
 
     // all 被点击时
