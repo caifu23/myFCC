c3779c85d3892433b01a6f505428eebf78efaef7
caifen <1224758467@qq.com>
2018-09-10 20:29:31 +0800

    Twitchtv JSON API

---
+++ fcc04/js/index.js
@@ -0,0 +1,62 @@
+$(function() {
+
+    $('#tvList').html('');
+    var tvChannels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","esl_sc2"];
+    var streamApi = "https://wind-bow.glitch.me/twitch-api/streams/";
+    var channelApi = 'https://wind-bow.glitch.me/twitch-api/channels/';
+    var arr = [];
+
+    // $.each(tvChannels, function(k, v) {
+    //     allStreamCall(v);   
+    // });
+
+    // 遍历tvChannels
+    tvChannels.forEach(function(v) {
+        allStreamCall(v);
+    });
+
+    console.log(arr);
+
+    function allStreamCall(v) {
+        //是否正在直播
+        $.getJSON(streamApi + v + "?callback=?",function(res){
+            if(res.stream == null) {
+                arr[v] = 'offline';
+            }else {
+                arr[v] = 'inline';
+            }
+        });
+        // 节目的相关信息
+        $.getJSON(channelApi + v + "?callback=?",function(data){
+            console.log(arr[v]  + '-------' + v);
+            if(arr[v] == 'inline') {
+                $('#tvList').prepend(" <li class='row "+ arr[v] +"'><div class='col-md-2' ><img src='" + data.logo +  "'/></div><div class='col-md-4' ><a href='"  + data.url +  "' target='_blank'>"+ data.display_name +"</a></div><div class='col-md-6'>" + data.display_name + " " + (data.status==null ? '' : data.status) + "</div></li>");
+
+            }else {
+                $('#tvList').append(" <li class='row "+ arr[v] +"'><div class='col-md-2' ><img src='" + data.logo +  "'/></div><div class='col-md-4' ><a href='"  + data.url +  "' target='_blank'>"+ data.display_name +"</a></div><div class='col-md-6'>" + "offline" + "</div></li>");
+            }
+        });
+     
+
+    }
+
+
+    // all 被点击时
+    $('#tvStatusAll').on('click', function() {
+        $('.offline').css('display','flex');
+        $('.inline').css('display','flex');
+    });
+
+    //tvOnline 被点击时
+    $('#tvOnline').on('click', function() {
+        $('.offline').css('display','none');
+        $('.inline').css('display','flex');
+    });
+
+    //tvOffline 被点击时
+    $('#tvOffline').on('click', function() {
+        $('.inline').css('display','none');
+        $('.offline').css('display','flex');
+    });
+
+});
---
+++ fcc04/readme.md
@@ -0,0 +1,29 @@
+<h1>Use the Twitchtv JSON API</h1>
+<pre>
+1、练习来源：<a href="https://freecodecamp.cn/challenges/use-the-twitchtv-json-api">Use the Twitchtv JSON API</a>
+
+2、功能：
+   1. 我可以看到 FreeCodeCamp 现在是否在Twitch.tv上直播
+   2. 我可以点击链接跳到 FreeCodeCamp 在Twitch.tv上的频道
+   3. 如果有人在直播，我可以看到他正在直播什么
+   4. 如果直播者关闭了他的账户，我会看到一个404提示页面。
+
+3、提示：
+   - 这有一个如何调用 Twitch.tv的JSONP API的示例：https://github.com/FreeCodeCamp/FreeCodeCamp/wiki/Front-End-Project-Use-the-Twitchtv-JSON-API.
+   - 这个文档有更多关于如何调用这个API的细节：https://github.com/justintv/Twitch-API/blob/master/v3_resources/streams.md#get-streamschannel
+   - 这有一个Twitch.tv 用户名数组，里面包含有通常正在直播写代码的人： ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"]
+
+4、api替换为这个
+    https://wind-bow.glitch.me/twitch-api/
+
+    https://wind-bow.glitch.me/twitch-api/streams/{channel}&&callback=?
+
+    https://wind-bow.glitch.me/twitch-api/channels/{channel}&&callback=?
+
+    https://wind-bow.glitch.me/twitch-api/streams/
+
+
+
+</pre>
+   
+   
