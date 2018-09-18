8376841f5799455fff75c5162a5b24398117cc0b
caifen <1224758467@qq.com>
2018-09-18 10:32:00 +0800

    location

---
+++ fcc02/js/index4.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('#wea').on('click', getLocation);
+    //获取位置
+    function getLocation() {
+        var locationArr = [];
+        if(!navigator.geolocation) {
+            console.log('您的浏览器不支持地理位置');
+            return ;
+        }
+
+        function success(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            locationArr.push(latitude, longitude);
+            console.log(locationArr);
+        };
+
+        function error() {
+            console.log('无法获取您的位置!');
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error);
+        
+    }
+
+
+
+    
+});
