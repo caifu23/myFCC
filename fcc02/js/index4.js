3914adbd751130d96c173aca7c0741eddc1c7357
caifen <1224758467@qq.com>
2018-09-18 11:29:35 +0800

    2

--- fcc02/js/index4.js
+++ fcc02/js/index4.js
@@ -10,6 +10,7 @@ $(function() {
     function getWeather() {
         getLocation();
         var num = locationArr[0];
+        $('.weather').append('----0'+ num);
         $('.weather').append('----1' + typeof num);
         $('.weather').append('----2' + parseInt(num));        
         $('.weather').append('-----3'+Math.floor(locationArr[0]));
